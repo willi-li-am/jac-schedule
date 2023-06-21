8689a339df77d3eaf8fa1bd0d26acdc9b5add1fa
William Li <52115161+Trollermaner@users.noreply.github.com>
2023-06-21 13:16:27 -0400

    LOGO

--- src/components/homePageTop.tsx
+++ src/components/homePageTop.tsx
@@ -1,8 +1,8 @@
-import bgPic from "../assets/jacschedulebackground.png"
+import bgPic from "../assets/JAC_ICON.png"
 
 export default function HomePageTop () {
     return(
-        <div className="flex flex-col bg-list items-center justify-center" style={{height: "60vh", backgroundImage: "url("+bgPic+")", backgroundSize: "33.333333vw"}}>
+        <div className="flex flex-col bg-list items-center justify-center" style={{height: "60vh", backgroundImage: "url("+bgPic+")", backgroundSize: "10vw"}}>
             <div className="text-white font-title mt-10 bg-darker p-2" style={{fontSize: "40px"}}>Mock Visual Schedule Builder for CEGEP</div>
             <div className="text-white italic bg-darker p-2" style={{fontSize: "20px", marginTop: ""}}>"inspiring quote about helping students"</div>
         </div>
