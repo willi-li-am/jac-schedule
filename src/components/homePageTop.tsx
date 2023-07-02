04d1b3cd7010e20cac87abfc1992c7a66b031925
William Li <52115161+Trollermaner@users.noreply.github.com>
2023-07-02 13:22:07 -0500

    changed the background

--- src/components/homePageTop.tsx
+++ src/components/homePageTop.tsx
@@ -2,9 +2,9 @@ import bgPic from "../assets/JAC_ICON.png"
 
 export default function HomePageTop () {
     return(
-        <div className="flex flex-col bg-list items-center justify-center" style={{height: "60vh", backgroundImage: "url("+bgPic+")", backgroundSize: "10vw"}}>
-            <div className="text-white font-title mt-10 bg-darker p-2" style={{fontSize: "40px"}}>Mock Visual Schedule Builder for CEGEP</div>
-            <div className="text-white italic bg-darker p-2" style={{fontSize: "20px", marginTop: ""}}>"inspiring quote about helping students"</div>
+        <div className="flex flex-col items-center justify-center" style={{height: "60vh", backgroundImage: "linear-gradient(to bottom, #819fcc, #323d54)"}}>
+            <div className="text-white font-title mt-10 p-2" style={{fontSize: "40px"}}>Mock Visual Schedule Builder for CEGEP</div>
+            <div className="text-white italic p-2" style={{fontSize: "20px", marginTop: ""}}>"inspiring quote about helping students"</div>
         </div>
     )
 }
